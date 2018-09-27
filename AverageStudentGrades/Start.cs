af7b4a5196a46c6b0cf18fb8ad9d71a81fce3e59
Emo <eiv.razgrad@gmail.com>
2018-09-27 23:43:57 +0300

    Add solved task from lab

---
+++ AverageStudentGrades/Start.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace AverageStudentGrades
+{
+    class Start
+    {
+        static void Main(string[] args)
+        {
+            int num = int.Parse(Console.ReadLine());
+            Dictionary<string, List<double>> data = new Dictionary<string, List<double>>();
+
+            while (num>0)
+            {
+                string[] info = Console.ReadLine().Split();
+                var studentName = info[0];
+                var grade = double.Parse(info[1]);
+
+                if (!data.ContainsKey(studentName))
+                {
+                    data.Add(studentName, new List<double>());
+                }
+                data[studentName].Add(grade);
+                num--;
+            }
+
+            foreach (var student in data)
+            {
+                Console.WriteLine($"{student.Key} -> {string.Join(" ", student.Value.Select(x => x.ToString("F2")))} (avg: {student.Value.Average().ToString("F2")})");
+            }
+
+
+        }
+    }
+}
---
+++ CountSameValueInArr/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CountSameValueInArr
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double[] input = Console.ReadLine().Split().Select(Double.Parse).ToArray();
+            Dictionary<double, int> data = new Dictionary<double, int>();
+            for (int n = 0; n < input.Length; n++)
+            {
+                if (!data.ContainsKey(input[n]))
+                {
+                    data[input[n]] = 1;
+                }
+                else
+                {
+                    data[input[n]]++;
+                }
+            }
+
+            foreach (var nums in data)
+            {
+                Console.WriteLine($"{nums.Key} - {nums.Value} times");
+            }
+        }
+    }
+}
