0075af54ab0953ec666747977e290ea7f6f8d7a8
Emo <eiv.razgrad@gmail.com>
2018-09-28 19:09:43 +0300

    Add

---
+++ ParkingLot/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ParkingLot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HashSet<string> dataForCars = new HashSet<string>();
+
+            string input = Console.ReadLine();
+
+            while (input != "END")
+            {
+
+                string[] parts = input.Split(", ".ToCharArray(), StringSplitOptions.RemoveEmptyEntries);
+                string command = parts[0];
+                string carNUmber = parts[1];
+                //
+                switch (command)
+                {
+                    case "IN":
+                        dataForCars.Add(carNUmber);
+                        break;
+                    case "OUT":
+                        dataForCars.Remove(carNUmber);
+                        break;
+                }
+                input = Console.ReadLine();
+            }
+            if (dataForCars.Count>0)
+            {
+                Console.WriteLine(string.Join("\n", dataForCars));
+            }
+            else
+            {
+                Console.WriteLine("Parking Lot is Empty");
+            }
+        }
+    }
+}
---
+++ SoftUniParty/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SoftUniParty
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HashSet<string> listOfVIP = new HashSet<string>();
+            HashSet<string> listOfNormal = new HashSet<string>();
+            string[] numArr = { "0", "1", "2", "3", "4", "5","6", "7", "8", "9"};
+           
+            string input = Console.ReadLine();
+
+            while (input != "PARTY")
+            {
+                var testForNum = input[0].ToString();
+                if (numArr.Contains(testForNum))
+                {
+                    listOfVIP.Add(input);
+                }
+                else
+                {
+                    listOfNormal.Add(input);
+                }
+                input = Console.ReadLine();
+            }
+
+            while (input != "END")
+            {
+
+                var testForNum = input[0].ToString();
+                if (numArr.Contains(testForNum))
+                {
+                    listOfVIP.Remove(input);
+                }
+                else
+                {
+                    listOfNormal.Remove(input);
+                }
+                input = Console.ReadLine();
+            }
+            var countGuestThatAreOut = listOfVIP.Count + listOfNormal.Count;
+            Console.WriteLine(countGuestThatAreOut);
+            if (listOfVIP.Count>0)
+            {
+                Console.WriteLine(string.Join("\n", listOfVIP));
+            }
+            if (listOfNormal.Count > 0)
+            {
+                Console.WriteLine(string.Join("\n", listOfNormal));
+            }
+
+
+
+        }
+    }
+}
